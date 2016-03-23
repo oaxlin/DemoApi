4d504e915b603e4551a836d37c5e563fc0be1b3f
Jason Terry <jterry@bluehost.com>
2016-03-23 10:14:10 -0600

    Add meta and validation

--- lib/DemoApi.pm
+++ lib/DemoApi.pm
@@ -3,8 +3,30 @@ use strict;
 use warnings;
 use base qw(Respite::Base);
 
+sub add__meta {
+    return {
+        desc => 'Adds value1 and value2',
+        args => {
+            value1 => {
+                desc => 'The first value',
+                type => 'NUM',
+                required => 1,
+            },
+            value2 => {
+                desc => 'The second value',
+                type => 'NUM',
+                default => 0,
+            },
+        },
+        resp => {
+            sum => 'The sum of value1 + value2',
+        },
+    };
+}
+
 sub add {
     my ($self,$args) = @_;
+    $self->validate_args($args);
     return {sum=>$args->{'value1'} + $args->{'value2'}};
 }
 
